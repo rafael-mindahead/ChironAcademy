43498be1a4509da673723dbcbdd0988e61486901
Vinicius Santos <vinialmeidacius@gmail.com>
2026-09-10 15:11:46 -0300

    feat: adicionado rotas para o CRUD dos alunos

---
+++ Backend/src/routes/alunoFormRoutes.js
@@ -0,0 +1,51 @@
+import express from 'express'
+
+import {
+    cadastrarAluno,
+    listarAlunos,
+    buscarAluno,
+    atualizarAluno,
+    excluirAluno
+} from '../controllers/alunoFormController.js'
+
+import { autenticarToken } from '../middlewares/authMiddleware.js'
+import { autorizarPerfis } from '../middlewares/authorizationMiddleware.js'
+
+const router = express.Router()
+
+router.post(
+    '/',
+    autenticarToken,
+    autorizarPerfis('GESTOR'),
+    cadastrarAluno
+)
+
+router.get(
+    '/',
+    autenticarToken,
+    autorizarPerfis('GESTOR'),
+    listarAlunos
+)
+
+router.get(
+    '/:id',
+    autenticarToken,
+    autorizarPerfis('GESTOR'),
+    buscarAluno
+)
+
+router.put(
+    '/:id',
+    autenticarToken,
+    autorizarPerfis('GESTOR'),
+    atualizarAluno
+)
+
+router.delete(
+    '/:id',
+    autenticarToken,
+    autorizarPerfis('GESTOR'),
+    excluirAluno
+)
+
+export default router
