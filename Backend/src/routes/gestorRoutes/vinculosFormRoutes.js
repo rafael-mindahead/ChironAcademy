2b55d913ee46138f2b614bd2b37822ac6376bc9a
Vinicius Santos <vinialmeidacius@gmail.com>
2026-09-11 21:02:34 -0300

    feat: implementado arquivo de rotas do CRUD de vinculos

---
+++ Backend/src/routes/gestorRoutes/vinculosFormRoutes.js
@@ -0,0 +1,51 @@
+import express from 'express'
+
+import {
+    listarOpcoes,
+    listarVinculos,
+    buscarVinculo,
+    criarVinculo,
+    excluirVinculo
+} from '../../controllers/vinculoFormController.js'
+
+import { autenticarToken } from '../middlewares/authMiddleware.js'
+import { autorizarPerfis } from '../middlewares/authorizationMiddleware.js'
+
+const router = express.Router()
+
+router.get(
+    '/opcoes',
+    autenticarToken,
+    autorizarPerfis('GESTOR'),
+    listarOpcoes
+)
+
+router.get(
+    '/',
+    autenticarToken,
+    autorizarPerfis('GESTOR'),
+    listarVinculos
+)
+
+router.get(
+    '/:idProfessorTurma',
+    autenticarToken,
+    autorizarPerfis('GESTOR'),
+    buscarVinculo
+)
+
+router.post(
+    '/',
+    autenticarToken,
+    autorizarPerfis('GESTOR'),
+    criarVinculo
+)
+
+router.delete(
+    '/:idProfessorTurma',
+    autenticarToken,
+    autorizarPerfis('GESTOR'),
+    excluirVinculo
+)
+
+export default router
